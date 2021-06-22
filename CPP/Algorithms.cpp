d9ec2884db0a182e4785f35d1635bde383430724
lamduong33 <lamthachduong@gmail.com>
2021-06-22 00:47:03 -0400

    Started C++ implementation of Johnson-Trotter Algorithm

---
+++ CPP/Algorithms.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+export module Algorithms;
+
+namespace Algorithms
+{
+
+/**
+ * Johnson-Trotter algorithm
+ * ----------------------------------------------------------
+ * input: n, the length of the sequence of numbers
+ * output: a vector containing all the permutations from 1 to n
+ */
+std::vector<int[]> johnsonTrotter(int n)
+{
+    std::vector<int[]> result;
+
+    return result;
+}
+}
