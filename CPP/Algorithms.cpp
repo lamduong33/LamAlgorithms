5dd79978d6cba3e1edd45de9a3ff079a99ccd65e
lamduong33 <lamthachduong@gmail.com>
2021-06-22 13:23:18 -0400

    Finished the Johnson-Trotter Algorithm for C++

--- CPP/Algorithms.cpp
+++ CPP/Algorithms.cpp
@@ -27,8 +27,36 @@ public:
 
         while (k != -1)
         {
+            for (int i = 0; i < n; i++)
+            {
+                if (numberSequence[i].first == k)
+                {
+                    int pointedIndex;
+                    if (numberSequence[i].second)
+                        pointedIndex = i - 1;
+                    else
+                        pointedIndex = i + 1;
 
+                    int tempNum = numberSequence[pointedIndex].first;
+                    bool tempArrow = numberSequence[pointedIndex].second;
+
+                    numberSequence[pointedIndex].first = numberSequence[i].first;
+                    numberSequence[pointedIndex].second = numberSequence[i].second;
+                    numberSequence[i].first = tempNum;
+                    numberSequence[i].second = tempArrow;
+                    break;
+                }
+            }
+            // Flip any thing that is greater than k, O(n)
+            for (int i = 0; i < n; i++) {
+                if (numberSequence[i].first > k) {
+                    numberSequence[i].second = !numberSequence[i].second;
+                }
+            }
+            result.push_back(getNumbers(numberSequence));
+            k = findK(numberSequence);
         }
+
         return result;
     }
 
@@ -37,7 +65,7 @@ public:
     static std::vector<int>
     getNumbers(const std::vector<std::pair<int, bool>> &numberSequence)
     {
-        auto result = new std::vector<int>(); // NOTE: allocate on heap just in case
+        auto result = new std::vector<int>(); // allocate on heap
         for (int i = 0; i < numberSequence.size(); i++)
         {
             (*result).push_back(numberSequence[i].first);
@@ -45,27 +73,33 @@ public:
         return *result;
     }
 
+    /* Find the largest mobile element. If the bool in the pair is true, the
+    arrow points left and points right if it's false.*/
     static int findK(std::vector<std::pair<int, bool>> numberSequence)
     {
         int k = -1;
         for (int i = 0; i < numberSequence.size(); i++)
         {
-
+            int num = numberSequence[i].first;
+            bool arrow = numberSequence[i].second;
+            if (i == 0)
+            {
+                if (!arrow && (num > numberSequence[i + 1].first))
+                    if (num > k) k = num;
+            }
+            else if (i != numberSequence.size() - 1)
+            {
+                if (((arrow && (num > numberSequence[i-1].first)) ||
+                    (!arrow && (num > numberSequence[i+1].first))))
+                    if(num > k) k = num;
+            }
+            else
+            {
+                if (arrow && (num > numberSequence[i - 1].first))
+                   if (num > k) k = num;
+            }
         }
         return k;
     }
 };
 
-int main(int argc, char *argv[])
-{
-    auto a = Algorithms::johnsonTrotter(3);
-    for (int i = 0; i < a.size(); i++)
-    {
-        for (int j = 0; j < a[i].size(); j++)
-        {
-            std::cout << a[i][j];
-        }
-        std::cout << std::endl;
-    }
-    return 0;
-}
