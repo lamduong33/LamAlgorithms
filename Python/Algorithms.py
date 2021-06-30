e9f0816cb766c1dc23768a51a2f6c1990653ba49
lamduong33 <lamthachduong@gmail.com>
2021-06-30 11:31:22 -0400

    Began implementation in Python.

---
+++ Python/Algorithms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy  # for performance reasons
+
+def johnsonTrotter(n: int) -> list:
+    """Johnson-Trotter Algorithm"""
+    k = n
+    result = list()
+    number_sequence = dict()
+    return result
