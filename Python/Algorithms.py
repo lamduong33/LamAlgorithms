348ea7b8284c80a78b83b0de1f665acc072f3768
Lam T Duong <lamthachduong@gmail.com>
2021-09-21 01:01:54 -0400

    Finished first implementation. It's late. Going to sleep.

--- Python/Algorithms.py
+++ Python/Algorithms.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
-
+#/usr/bin/env python3
+#! @author: Lam Duong
+# Algorithm implementations for Lam's Algorithm channel
 
 def johnsonTrotter(n: int) -> list:
     """Johnson-Trotter Algorithm:
@@ -15,19 +16,27 @@ def johnsonTrotter(n: int) -> list:
         arrows.append(True)
     result.append(numbers)
 
-    while (hasMobileElement(numbers, arrows) != -1):
-        result.append("Lam")
+    k = largestMobileElement(numbers, arrows)
+    while (k != -1):
+        k = largestMobileElement(numbers, arrows)
+        direction = -1 if numbers[k] else 1
+        swapElements(numbers, k, k+direction)
+        # Reverse the direction of all elements larger than k
+        for i in range(len(numbers)):
+            if (numbers[i] > numbers[k]):
+                arrows[i] = not arrows[i]
+        result.append(numbers)
 
     return result
 
-
-def hasMobileElement(numbers: list, arrows: list) -> int:
+def largestMobileElement(numbers: list, arrows: list) -> int:
     """Helper function for Johnson-Trotter:
-    Check to see if the number sequence has a mobile element.
+    Check to see if the number sequence has a mobile element by getting the
+    largest mobile element. If there are no mobile elements, return -1.
     To ensure the runtime of the algorithm, this function will need O(n)
-    runtime. Return the index of the largest mobile element, k is -1 if there is
-    no mobile element"""
-    k = -1
+    runtime. """
+
+    k = -1 # No mobile element state
     # Only return k if the a number points to a smaller adjacent number.
     for index in range(len(numbers)):
         if (((arrows[index] == True)
@@ -41,5 +50,11 @@ def hasMobileElement(numbers: list, arrows: list) -> int:
             elif (numbers[index] > numbers[k]):
                 k = index
     return k
+
+def swapElements(t_list: list, index1: int, index2: int):
+    """ Helper function to swap elements in a list based on indices. """
+    temp = t_list[index1]
+    t_list[index1] = t_list[index2];
+    t_list[index2] = temp
 
 johnsonTrotter(3)
