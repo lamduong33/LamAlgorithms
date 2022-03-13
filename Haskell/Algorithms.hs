268ad507293c94883a371514d1b777a04c66ce1a
Lam T Duong <lamthachduong@gmail.com>
2022-03-13 14:43:09 -0400

    feat: Quicksort algorithm added.

--- Haskell/Algorithms.hs
+++ Haskell/Algorithms.hs
@@ -3,5 +3,13 @@ greatestCommonDivisor :: Integer -> Integer -> Integer
 greatestCommonDivisor a 0 = abs a
 greatestCommonDivisor a b = greatestCommonDivisor b (mod a b)
 
+-- Quick sort algorithm
+qsort :: [Integer] -> [Integer]
+qsort [] = []
+qsort (n : ns) = qsort smaller ++ [n] ++ qsort bigger
+  where
+    smaller = [a | a <- ns, a <= n]
+    bigger = [b | b <- ns, b > n]
+
 main :: IO ()
 main = print (greatestCommonDivisor 74224 1232)
