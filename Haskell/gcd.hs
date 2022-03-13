87f570c201e00d737fbec7148bf1f06bf2476598
Lam T Duong <lamthachduong@gmail.com>
2022-03-13 04:50:07 -0400

    feat: greatest common divisor for Haskell.

---
+++ Haskell/gcd.hs
@@ -0,0 +1,7 @@
+-- Euclid's algorithm
+greatestCommonDivisor :: Integer -> Integer -> Integer
+greatestCommonDivisor a 0 = abs a
+greatestCommonDivisor a b = greatestCommonDivisor b (mod a b)
+
+main :: IO ()
+main = print (greatestCommonDivisor 74224 1232)
